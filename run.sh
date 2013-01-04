bfe864ba9be92d6a3f362fbfb706e16aba66ff04
Michael R. Hines <michael@hinespot.com>
2013-01-04 01:44:11 -0500

    added nice to helper script

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 # Just a helper script for running stuff
 
-file="../st.mpg"
+#file="../st.mpg"
 #file="../st.m2ts"
-#file="/storage/recordings_readable/bytitle/Star Trek- First Contact/20121004-2100-2330- Untitled.mpg"
+file="/storage/recordings_readable/bytitle/Star Trek- First Contact/20121004-2100-2330- Untitled.mpg"
 #file="sample.mpg"
 #file="sample2.mpg"
 #file="sample3.mpg"
@@ -21,11 +21,11 @@ make
 if [ $? -eq 0 ] ; then 
 	if [ x"$1" == xslave ] ; then
 		#gdb ~/mythtv_to_dvd/luvi/luvi_slave -ex "run $2 1264"
-		~/mythtv_to_dvd/luvi/luvi_slave $2 1264
+		nice -n 19 ~/mythtv_to_dvd/luvi/luvi_slave $2 1264
 	else
 		#gdb ~/mythtv_to_dvd/luvi/luvi_master -ex "run \"${file}\" \"$video\" 1264 1"
-		#~/mythtv_to_dvd/luvi/luvi_master "${file}" "$video" 1264 0 # build index
-		#~/mythtv_to_dvd/luvi/luvi_master "${file}" "$video" 1264 0 # local transcode
-		~/mythtv_to_dvd/luvi/luvi_master "${file}" "$video" 1264 1 # slave transcode
+		#nice -n 19 ~/mythtv_to_dvd/luvi/luvi_master "${file}" "$video" 1264 0 # build index
+		#nice -n 19 ~/mythtv_to_dvd/luvi/luvi_master "${file}" "$video" 1264 0 # local transcode
+		nice -n 19 ~/mythtv_to_dvd/luvi/luvi_master "${file}" "$video" 1264 1 # slave transcode
 	fi
 fi
